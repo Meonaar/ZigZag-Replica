a95b2a78c184c107f38af2b1b414c7b4b0da08f7
Meonaar <48944686+Meonaar@users.noreply.github.com>
2019-10-28 16:51:48 +0500

    Download project

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (player.transform.position.y < 0.5)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float force = 4f;
+    float moveAmount;
+    Vector3 direction;
+
+    int score = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (direction == Vector3.forward)
+            {
+                direction = Vector3.left;
+            }
+            else
+            {
+                direction = Vector3.forward;
+            }
+        }
+        moveAmount = force * Time.deltaTime;
+        transform.Translate(direction * moveAmount);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            score++;
+            scoreText.text = score.ToString();
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
+    }
+}
